fa2f6cf8c0495c0572a9b3dbbf781683ff785f26
Wasabi Fan <wasabifan@outlook.com>
2016-07-06 21:46:36 -0700

    Add ability to backproject with saturation as well as hue

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
 #include <memory>
+#include <iostream>
 #include "TimeUtils.h"
 
 using namespace cv;
@@ -15,11 +16,10 @@ using namespace cv;
 
 #define CAPTURE_CHECKPOINT "Capture"
 #define THRESH_CHECKPOINT "Threshold"
-#define MIX_CHANNELS_CHECKPOINT "Mix channels"
 #define BACK_PROJECT_CHECKPOINT "Back project"
 #define CAM_SHIFT_CHECKPOINT "Cam shift"
 #define BLOB_DETECT_CHECKPOINT "Blob detect"
-#define RENDER_CHECKPOINT "Render"
+#define RENDER_CHECKPOINT "Render GUI"
 
 struct TargetBoundaryInfo
 {
@@ -36,6 +36,11 @@ bool operator==(KeyPoint const& lhs, KeyPoint const& rhs)
         && lhs.response == rhs.response
         && lhs.octave == rhs.octave
         && lhs.class_id == rhs.class_id;
+}
+
+static void setBoolCallback(int pos, void* userData)
+{
+    *(bool*)userData = pos;
 }
 
 static void onMouse(int event, int x, int y, int flags, void* userData)
@@ -140,11 +145,12 @@ int main()
         sMin = 0, sMax = 255,
         vMin = 0, vMax = 255;
 
+    bool includeSatInHist = false;
+
     TimerManager timers;
     timers.addCheckpoint(CAPTURE_CHECKPOINT);
     timers.addCheckpoint(THRESH_CHECKPOINT, CAPTURE_CHECKPOINT);
-    timers.addCheckpoint(MIX_CHANNELS_CHECKPOINT, THRESH_CHECKPOINT);
-    timers.addCheckpoint(BACK_PROJECT_CHECKPOINT, MIX_CHANNELS_CHECKPOINT);
+    timers.addCheckpoint(BACK_PROJECT_CHECKPOINT, THRESH_CHECKPOINT);
     timers.addCheckpoint(BLOB_DETECT_CHECKPOINT, BACK_PROJECT_CHECKPOINT);
     timers.addCheckpoint(CAM_SHIFT_CHECKPOINT, BLOB_DETECT_CHECKPOINT);
     timers.addCheckpoint(RENDER_CHECKPOINT, CAM_SHIFT_CHECKPOINT);
@@ -162,6 +168,7 @@ int main()
     createTrackbar("S max", "Config", &sMax, 255);
     createTrackbar("V min", "Config", &vMin, 255);
     createTrackbar("V max", "Config", &vMax, 255);
+    createTrackbar("Enable sat", "Config", nullptr, 1, setBoolCallback, &includeSatInHist);
 
     Rect* selectedTarget = nullptr;
     setMouseCallback("Source", onMouse, &selectedTarget);
@@ -191,14 +198,18 @@ int main()
     // 1 second prune time threshold
     int64 targetPruneTime = (int64)(cv::getTickFrequency() * 1);
 
-    float hueRange[] = { 0, 180 };
-    const float* hueRangePtr = hueRange;
-    int numBins = 32;
+    int histogramChannels[] = { 0, 1 };
+    float hueRange[] = { 0, 179 };
+    float satRange[] = { 0, 255 };
+    const float* histogramRanges[] = { hueRange, satRange };
+    int numHueBins = 30;
+    int numSatBins = 32;
+    int histogramNumBins[] = { numHueBins, numSatBins };
 
     timers.printTableHeader();
 
     Mat sourceFrame, hsvFrame, threshFrame, backprojFrame, histogramRender;
-    Mat hueChannel, targetHueHistogram;
+    Mat targetColorHistogram;
     for(uint32_t frameNumber = 0; capture.isOpened(); frameNumber++)
     {
         timers.start();
@@ -208,39 +219,40 @@ int main()
         timers.markCheckpoint(CAPTURE_CHECKPOINT);
 
         //medianBlur(sourceFrame, sourceFrame, 5);
+        // TODO: Separate color conversion from thresh
         cvtColor(sourceFrame, hsvFrame, CV_BGR2HSV);
         inRange(hsvFrame, Scalar(hMin, sMin, vMin), Scalar(hMax, sMax, vMax), threshFrame);
 
         timers.markCheckpoint(THRESH_CHECKPOINT);
 
-        int channelMap[] = { 0, 0 };
-        hueChannel.create(hsvFrame.size(), hsvFrame.depth());
-        mixChannels(&hsvFrame, 1, &hueChannel, 1, channelMap, 1);
-
-        timers.markCheckpoint(MIX_CHANNELS_CHECKPOINT);
-
         if (selectedTarget != nullptr && selectedTarget->area() > 0)
         {
             // TODO: Transition selection code to smart_ptr
             //printf("Updating target histogram with rect (x: %d, y: %d) (w: %d, h: %d)\r\n", selectedTarget->x, selectedTarget->y, selectedTarget->width, selectedTarget->height);
-            Mat hueRoi = Mat(hueChannel, *selectedTarget);
+            Mat sourceRoi = Mat(hsvFrame, *selectedTarget);
             Mat maskRoi = Mat(threshFrame, *selectedTarget);
 
-            calcHist(&hueRoi, 1, 0, maskRoi, targetHueHistogram, 1, &numBins, &hueRangePtr);
-            normalize(targetHueHistogram, targetHueHistogram, 0, 255, NORM_MINMAX);
+            calcHist(&sourceRoi, 1, histogramChannels, maskRoi, targetColorHistogram, includeSatInHist ? 2 : 1, histogramNumBins, histogramRanges);
+            normalize(targetColorHistogram, targetColorHistogram, 0, 255, NORM_MINMAX);
 
-            histogramRender = Mat::zeros(200, 320, CV_8UC3);
-            renderHueHistogram(targetHueHistogram, histogramRender, numBins);
-            imshow("Histogram render", histogramRender);
+            // TODO: Re-write histogram render for 2d histogram with sat
+            std::cout << targetColorHistogram << std::endl;
+
+            if (!includeSatInHist)
+            {
+                histogramRender = Mat::zeros(200, numHueBins * 10, CV_8UC3);
+                renderHueHistogram(targetColorHistogram, histogramRender, numHueBins);
+                imshow("Histogram render", histogramRender);
+            }
 
             delete selectedTarget;
             selectedTarget = nullptr;
         }
 
 
-        if (!targetHueHistogram.empty())
+        if (!targetColorHistogram.empty() && (targetColorHistogram.cols == 1) == !includeSatInHist)
         {
-            calcBackProject(&hueChannel, 1, 0, targetHueHistogram, backprojFrame, &hueRangePtr);
+            calcBackProject(&hsvFrame, 1, histogramChannels, targetColorHistogram, backprojFrame, histogramRanges);
             backprojFrame &= threshFrame;
 
             erode(backprojFrame, backprojFrame, Mat(), Point(-1, -1), 3);
@@ -260,7 +272,8 @@ int main()
             for (auto trackedTarget : trackedTargets)
             {
                 // TODO: Tune cam shift params
-                trackedTarget->lastTrackedPose = CamShift(backprojFrame, *trackedTarget->targetBounds.get(), TermCriteria(TermCriteria::COUNT | TermCriteria::EPS, 10, 1));
+                if(trackedTarget->targetBounds->area() > 0)
+                    trackedTarget->lastTrackedPose = CamShift(backprojFrame, *trackedTarget->targetBounds.get(), TermCriteria(TermCriteria::COUNT | TermCriteria::EPS, 10, 1));
             }
 
             timers.markCheckpoint(CAM_SHIFT_CHECKPOINT);
@@ -282,13 +295,6 @@ int main()
             timers.markCheckpoint(BLOB_DETECT_CHECKPOINT);
             timers.markCheckpoint(RENDER_CHECKPOINT);
         }
-
-        /*cv::threshold(workingFrame, workingFrame, 150, 255, THRESH_TOZERO);
-
-        std::vector<std::vector<Point> > contours(100);
-        cv::findContours(workingFrame, contours, RETR_LIST, CHAIN_APPROX_SIMPLE);
-
-        drawContours(sourceFrame, contours, -1, Scalar(255, 0, 255), 1);*/
 
         timers.stop();
         timers.printTableRow();
